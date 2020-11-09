eb08dfa1502afe802d5df40430c893b43b9d83a2
EyalMorad <72889776+EyalMorad@users.noreply.github.com>
2020-11-09 12:20:24 +0200

    Delete main.js

--- Intro-To-JSON/Exercise1&2/main.js
+++
@@ -1,6 +0,0 @@
-let recipe = ingredients
-
-console.log(recipe.ingredients.filter(i => i.name == "garlic powder")[0].count) //should print 2
-console.log(recipe.healthStatus) //should print true
-console.log(recipe.caloriesValue) //should print 250
-console.log(recipe.directions[0]) //should print "Cut potatoes into half inch thick slices"
