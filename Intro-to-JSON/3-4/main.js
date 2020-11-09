a9891d240c35c43f19d0280829e6848c3e4fb0fb
EyalMorad <72889776+EyalMorad@users.noreply.github.com>
2020-11-09 12:53:13 +0200

    Add files via upload

---
+++ Intro-to-JSON/3-4/main.js
@@ -0,0 +1,20 @@
+let animals = {
+    "name" : "cow",
+    "weight": "150",
+    "weightUnit": "kg",
+    "livingPlace": "fields",
+    "endangered": false,
+    "foodKinds" : [
+        {"name":"plants"
+        "percentage":30},
+        {"name":"fruits"
+        "percentage":20},
+        {"name":"grass"
+        "percentage":50}
+    ],
+    "animalsThatEatIt": [
+        "wolf","hyena" 
+    ]
+    
+    "howDelicious" : 5
+}
